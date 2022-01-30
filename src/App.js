cb86e72ebd421c97af5aee88c3761738aaf769bc
fuadmiazi <fuad.hasan.1997@gmail.com>
2022-01-30 06:59:47 +0600

    Step 1 Done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person= {
+    name: "Fuad",
+    job: "Gamer"
+}
+var person2= {
+  name: "Nawal",
+  job: "Software Engineer"
+}
+var style={
+  color: 'red',
+  backgroundColor: 'yellow'
+}
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +21,10 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
+        <p>My First React Paragraph</p>
+        <h1 className="" style={style} >my heading: {person.name +" is a: "+ person.job}</h1>
+        <h1 style={{color: 'black',
+        backgroundColor: 'cyan'}}>Friend: {person2.name+" is a: "+ person2.job}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
